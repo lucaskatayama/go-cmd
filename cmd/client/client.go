c174eea2838d2cc472dc2ef6cfc593ce1c9fddff
Lucas Katayama <lucas.katayama@mercadobitcoin.com.br>
2020-02-14 11:06:10 -0300

    cmd

---
+++ cmd/client/client.go
@@ -0,0 +1,7 @@
+package client
+
+import "fmt"
+
+func Exec() {
+	fmt.Println("Hello")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/lucaskatayama/go-cmd/cmd/client"
+
+func main() {
+	client.Exec()
+}
